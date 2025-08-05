d2791117c139d0038296c8137639c50129a40f36
星空幻梦_152 <90498761+gunfub@users.noreply.github.com>
2025-08-06 02:44:31 +0800

    updata v1.0 module source

--- modules/action.sh
+++ modules/action.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 
-rm -f /data/adb/ADPDEDT/log/ADPDEDT.log
+# rm -f /data/adb/ADPDEDT/log/ADPDEDT.log
 sh /data/adb/modules/ADPDEDT/main.sh 2>&1 | tee /data/adb/ADPDEDT/log/ADPDEDT.log
 exit 0
