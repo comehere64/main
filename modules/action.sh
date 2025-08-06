74fa2448f56b5c04720f5f73eba7cef9844a0398
星空幻梦_152 <90498761+gunfub@users.noreply.github.com>
2025-08-06 13:21:01 +0800

    update v1.3 module source

--- modules/action.sh
+++ modules/action.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-echo "手动触发，开始执行......"
 rm -f /data/adb/ADPDEDT/log/ADPDEDT.log
-sh /data/adb/modules/ADPDEDT/main.sh 2>&1 | tee /data/adb/ADPDEDT/log/ADPDEDT.log
+echo "手动触发，开始执行......" | tee /data/adb/ADPDEDT/log/ADPDEDT.log
+sh /data/adb/modules/ADPDEDT/main.sh 2>&1 | tee -a /data/adb/ADPDEDT/log/ADPDEDT.log
 exit 0
--- modules/service.sh
+++ modules/service.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
-echo "开机自动触发，开始执行......"
-sleep 60
 rm -f /data/adb/ADPDEDT/log/ADPDEDT.log
-sh /data/adb/modules/ADPDEDT/main.sh 2>&1 | tee /data/adb/ADPDEDT/log/ADPDEDT.log
+echo "开机自动触发，开始执行......" | tee /data/adb/ADPDEDT/log/ADPDEDT.log
+sleep 60
+sh /data/adb/modules/ADPDEDT/main.sh 2>&1 | tee -a /data/adb/ADPDEDT/log/ADPDEDT.log
 exit 0
